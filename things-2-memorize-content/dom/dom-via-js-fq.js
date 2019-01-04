e648efc73d3f50c69b87598c998091f07d0f2b9c
Apollo Tang <tang.apollo@gmail.com>
2019-01-03 21:55:44 -0500

    update

--- things-2-memorize-content/dom/dom-via-js-fq.js
+++ things-2-memorize-content/dom/dom-via-js-fq.js
@@ -3,13 +3,31 @@
   const el = document.getElementById('SomeId');
   const el = document.getElementsByTagName('body')[0];
 
+
 // Creating element
   const myButton = document.createElement('button');
 
+
+// Class name
+  const container = document.createElement('div');
+  container.className='my-classname';
+  document.body.append(container)
+
+
 // Body
+  const box = document.createElement('div');
   document.body.appendChild(box);
 
+  const myBox = document.createElement('div');
   const body = document.getElementsByTagName('body')[0];
-  body.appendChild(myButton)
+  body.appendChild(myBox)
+
+
+// Creating TextNode
+  const text ='My Text'
+  const container = document.createElement('div');
+  const textNode = document.createTextNode(text);
+  container.appendChild(textNode)
+  document.body.append(container)
 
 
